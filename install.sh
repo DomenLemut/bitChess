66448e652c77afc16ad9e3de320c5f99ddbc39f6
Domen Lemut <dl0300@student.uni-lj.si>
2024-03-02 00:10:05 +0100

    saw and fixed

--- install.sh
+++ install.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-MAIN_PROGRAM="./src/main.c"
-
-
 cd src
 
-INCLUDE_FILE="./includes.h"
-
-headers=$(find -name "*.h" ! -name "includes.h")
-
-echo "// Automatically generated includes.h file" > $INCLUDE_FILE
-
-for header in $headers
-do
-    echo "#include \"$header\"" >> $INCLUDE_FILE
-done
-
-cd ..
+MAIN_PROGRAM="./main.c"
 
 programs=$(find -name "*.c" ! -name "main.c")
+
+echo $programs
 
 gcc -o chess $programs $MAIN_PROGRAM
